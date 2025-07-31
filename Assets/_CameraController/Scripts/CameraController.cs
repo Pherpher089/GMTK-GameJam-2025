6aa49f15f030b70b1426ea1211169fc5a4a36327
Pherpher089 <christutor089@gmail.com>
2025-07-30 21:49:19 -0700

    adding basic character controller

---
+++ Assets/_CameraController/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject m_Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Target != null)
+        {
+            // === POSITION FOLLOWING ===
+            Vector3 targetPosition = m_Target.transform.position;
+            targetPosition.y = transform.position.y; // Keep the camera at the same height
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 3f);
+
+            // === ROTATION FOLLOWING ===
+            // Get the target's forward direction projected onto the horizontal plane
+            Vector3 targetForward = m_Target.transform.forward;
+            targetForward.y = 0f;
+
+            if (targetForward.sqrMagnitude > 0.001f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(targetForward);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 3f);
+            }
+        }
+    }
+}
--- Assets/_Character/Scripts/ThirdPersonUserControl.cs
+++ Assets/_Character/Scripts/ThirdPersonUserControl.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class ThirdPersonUserControl : MonoBehaviour
 {
+    ThirdPersonWitchController m_Character;
+    public float speed = 5f;
+    // Start is called before the first frame update
     void Start()
     {
-        
+        m_Character = GetComponent<ThirdPersonWitchController>();
     }
 
-    void Update()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float accelerate = Input.GetAxis("Accelerate");
+        float brake = Input.GetAxis("Brake");
+        bool jump = Input.GetButtonDown("Jump");
+
+        m_Character.Move(horizontal, accelerate, brake, jump);
     }
 }
